5158e23e7921a988052f386b47be78f2559462be
fa-mc <marcus.chen02@gmail.com>
2019-11-17 04:19:19 +0000

    bug fix for db con

--- common/db.py
+++ common/db.py
@@ -3,10 +3,10 @@ from sqlalchemy import create_engine as __create_engine
 
 
 def create_engine_for_db(*args, **kwargs):
-    username = os.env['DB_USER']
-    password = os.env['DB_PASS']
-    host = os.env['DB_HOST']
-    schema = os.env['DB_SCHEMA']
+    username = os.environ['DB_USER']
+    password = os.environ['DB_PASS']
+    host = os.environ['DB_HOST']
+    schema = os.environ['DB_SCHEMA']
     
     DB_URL = f"mysql://{username}:{password}@{host}/{schema}"
     return __create_engine(DB_URL, *args, **kwargs)
